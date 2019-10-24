4a4d9c3b3ce48559fbefa95d17269bd0182a0fe4
randybu <randybuntar@gmail.com>
2019-10-24 11:56:38 +0700

    First commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public Main(){
+		
+	}
+	public static void main(String[] args) {
+		String nama;
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Nama : ");
+		nama = scan.nextLine();
+		System.out.println("Nama anda adalah : " + nama);
+	}
+
+}
